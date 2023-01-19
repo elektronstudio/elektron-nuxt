0ed90a7a56daab78e9853f2f2af613b6af1d31cc
sven-ra <mail@svenraju.net>
2023-01-19 16:12:14 +0200

    feat: buttons for memento events

---
+++ components/MementoButtons.vue
@@ -0,0 +1,58 @@
+<script lang="ts" setup>
+type Props = {
+  controls: {
+    label: string;
+    channel: string;
+    type: string;
+  }[];
+};
+
+const { controls } = defineProps<Props>();
+</script>
+
+<template>
+  <div class="MementoButtons">
+    <button v-for="control in controls" className="MementoButton">
+      <svg>
+        <text x="50%" y="50%" dominant-baseline="central" text-anchor="middle">
+          {{ control.label }}
+        </text>
+      </svg>
+    </button>
+  </div>
+</template>
+
+<style>
+.MementoButtons {
+  display: flex;
+  /* gap: var(--p-5); */
+  justify-content: space-between;
+  padding: var(--p-5);
+  flex-wrap: wrap;
+}
+.MementoButton {
+  position: relative;
+  background-color: var(--accent);
+  border-radius: var(--rounded-DEFAULT);
+  width: 30%;
+  aspect-ratio: 1 / 1;
+}
+.MementoButton svg {
+  /* position: absolute; */
+  /* top: 0; */
+  /* left: 0; */
+  width: 100%;
+  height: 100%;
+}
+.MementoButton svg text {
+  font-size: 10vw;
+}
+.MementoButton:hover {
+  filter: brightness(0.8);
+}
+@media only screen and (min-width: 600px) {
+  .MementoButton svg text {
+    font-size: 2.6vw;
+  }
+}
+</style>
---
+++ pages/projects/memento/[event_slug]/live.vue
@@ -0,0 +1,120 @@
+<script setup lang="ts">
+import { Urgency } from "~~/types";
+
+const route = useRoute();
+const slug = route.params.event_slug as string;
+const { lang } = useLang();
+const { data: event, error } = await useEventBySlug(slug as string);
+
+useHead({
+  title: `${event.value.title} – elektron.art`,
+});
+
+const videostreams = getVideostreams(event.value.streamkey);
+
+const draggables = useDraggables({
+  video: {
+    titles: ["Stream", "Stream"],
+    contentType: "video",
+    initialX: 0,
+    initialY: 1,
+    tilesWidth: 15,
+    maximisable: true,
+    hideTitleBarOnIdle: true,
+  },
+  chat: {
+    contentType: "chat",
+    initialX: 16,
+    initialY: 1,
+    tilesWidth: 4,
+    tilesHeight: 8,
+  },
+  about: {
+    tilesWidth: 8,
+    tilesHeight: 4,
+    initialX: 1,
+    initialY: 6,
+  },
+  controls: {
+    tilesWidth: 4,
+    // tilesHeight: 2,
+    initialX: 10,
+    initialY: 6,
+  },
+});
+
+const controls = parseControls(event.value.controls);
+const hasTicket = ref<boolean>(false);
+const dialogState = ref<boolean>(true);
+
+// @TODO: Move this to processing level
+const { urgency } = useDatetime(event.value.start_at, event.value.end_at);
+const urgencyLabel = computed(() => {
+  if (urgency.value === ("future" as Urgency)) {
+    return ["upcoming", "tulemas"];
+  } else if (urgency.value === ("soon" as Urgency)) {
+    return ["soon", "varsti"];
+  } else if (urgency.value === ("now" as Urgency)) {
+    return ["live", "live"];
+  } else {
+    return ["new", "uus"];
+  }
+});
+
+const noTicketDraggables = useDraggables({
+  preview: {
+    titles: urgencyLabel.value,
+    initialX: 6,
+    initialY: 2,
+    tilesWidth: 8,
+    // tilesHeight: 5,
+  },
+});
+
+onMounted(() => {
+  const fientaValidate = getTicketableStatus([event.value]);
+
+  if (fientaValidate.status !== "REQUIRES_TICKET") {
+    hasTicket.value = true;
+  }
+});
+</script>
+
+<template>
+  <div>
+    <BackToEvent :event="event" />
+    <EBreadBoard v-if="!hasTicket">
+      <DraggableHoc v-bind="noTicketDraggables.preview">
+        <EventPreview
+          :event="event"
+          :dialog-state="dialogState"
+          :is-event="true"
+          @close-dialog="dialogState = false"
+        />
+      </DraggableHoc>
+      <DraggablesDock :draggables="noTicketDraggables" />
+    </EBreadBoard>
+    <EBreadBoard v-else>
+      <DraggableHoc v-bind="draggables.video" v-if="event.streamUrl">
+        <Videostream :url="event.streamUrl" />
+      </DraggableHoc>
+      <DraggableHoc v-bind="draggables.video" v-else-if="videostreams?.length">
+        <Videostream :url="videostreams[0].url" />
+      </DraggableHoc>
+      <DraggableHoc v-bind="draggables.chat">
+        <Chat :channel="slug" />
+      </DraggableHoc>
+      <DraggableHoc v-bind="draggables.about">
+        <EStack style="padding: var(--p-5)">
+          <ETitle size="lg">Live event: {{ event.title }}</ETitle>
+          <EContent :content="event.live_contents[lang]" />
+        </EStack>
+      </DraggableHoc>
+      <DraggableHoc v-bind="draggables.controls">
+        <MementoButtons :controls="controls" />
+      </DraggableHoc>
+
+      <DraggablesDock :draggables="draggables" />
+    </EBreadBoard>
+  </div>
+</template>
